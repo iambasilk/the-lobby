b7587e6066fc16cd5b34976b232cfe529aaa37bc
Basil K <basilk87@gmail.com>
2023-09-08 00:56:27 +0530

    Pilot commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// src/components/App.js
+import React from "react";
+import { CasinoProvider } from "./context/CasinoContext";
+import Navbar from "./components/Navbar";
+import GameList from "./components/GameList";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CasinoProvider>
+      <div className="App container">
+        <Navbar />
+        <GameList />
+      </div>
+    </CasinoProvider>
   );
 }
 
